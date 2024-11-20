d2b5ed80d04880609979ea8d1647aaeb2dd0e654
HajizadaIlgar <ilqarh-ab108@code.edu.az>
2024-11-21 02:48:16 +0400

    Uniclo Task

---
+++ MyUnicloTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ab108Uniqlo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
